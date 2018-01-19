ef65ca54c59c705e9e8737cfffa858635f3b39ea
Alessandro Sangiorgi <mail@alessandrosangiorgi.net>
2018-01-19 17:42:18 +0100

    Add files via upload

---
+++ fakelogmaker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {1..1000}
+do
+	sleep 1
+	if [ $((1 + RANDOM % 10)) -gt 5 ]
+		then
+		echo " $(date +%s) $(hostname) TOPIC$((1 + RANDOM %4)) with value $(( 1 + RANDOM % 50))" >> /root/fake.log
+	else
+		echo "FUFFAAAA" >> /root/fake.log
+	fi
+done
