4f31e3bb344a521054166a0995ea4d0ed250d6dd
fulvius31 <alessandrosangiorgi31@gmail.com>
2018-02-10 22:36:04 +0100

    - left glassfish for the moment (bad conn)

--- glassfish4/bin/deployment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-for i in {200..204}
-do
-	./asadmin deploy --user=admin --passwordfile=/home/fulvius/SistemiDistribuiti/SistemiDistribuiti/GfAdminPassword  --host=10.123.123.$i /home/fulvius/SistemiDistribuiti/SistemiDistribuiti/NodeApplication/dist/NodeApplication.ear 
-done
